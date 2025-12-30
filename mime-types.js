c1c3b300d946e4daa2d42fa37db453728b4d8117
Shashi Shekhar <shshekhar93@gmail.com>
2025-12-30 07:28:31 +0000

    Better mime type handling using mime

--- mime-types.js
+++ mime-types.js
@@ -1,25 +1,7 @@
-const { extname } = require('path');
+const { default: mime } = require('mime/lite');
 
-const MIME_TYPES = {
-  default: "application/octet-stream",
-  html: "text/html; charset=UTF-8",
-  txt: "text/plain",
-  js: "text/javascript",
-  css: "text/css",
-  png: "image/png",
-  jpg: "image/jpeg",
-  gif: "image/gif",
-  ico: "image/x-icon",
-  svg: "image/svg+xml",
-};
-
-function getMimeType(path) {
-  // if extension is provided, return fast 
-  if(MIME_TYPES[path]) {
-    return MIME_TYPES[path]
-  };
-
-  return MIME_TYPES[extname(path).substring(1).toLowerCase()] ?? MIME_TYPES.default;
+function getMimeType(pathOrExt) {
+  return mime.getType(pathOrExt);
 }
 
 module.exports = {
